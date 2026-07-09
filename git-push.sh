95dc8a91fbf3143408f1448158e33c9bc787191e
Mubasher Hassan <arid1182@gmail.com>
2026-07-09 07:43:43 +0000

    git-push

--- git-push.sh
+++ git-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get commit message from argument, default to "Welcome to StackSimplify"
-COMMIT_MSG="${1:-Welcome to StackSimplify}"
+COMMIT_MSG="${1:-Welcome to Capclouds.com}"
 
 echo "...........ADDING ALL FILES ..........."
 git add .
